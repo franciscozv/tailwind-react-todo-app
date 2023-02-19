574b3af8550ce411fa0e6fea0964e8f1ae0203ea
franciscozv <javierzambranovaldes@gmail.com>
2023-02-19 01:24:29 -0300

    detalle input tailwind

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -14,7 +14,8 @@ const TodoCreate = ({createTodo}) => {
   return (
         <form onSubmit={handleSubmitAddTodo} className="flex gap-4 bg-white rounded-md items-center py-4 px-4 dark:bg-gray-800 transition-all duration-1000">
             <span className="rounded-full border-2 h-5 w-5 inline-block"></span>
-            <input type="text" placeholder="Create a new todo..." className="w-full text-gray-400 outline-none dark:bg-gray-800 transition-all duration-1000 dark:placeholder:text-gray-500" value={title}
+            <input type="text" placeholder="Create a new todo..." className="w-full text-gray-600
+            outline-none dark:bg-gray-800 transition-all duration-1000 dark:placeholder:text-gray-500 dark:text-gray-400" value={title}
             onChange={(e) => setTitle(e.target.value)}
             />
         </form>
